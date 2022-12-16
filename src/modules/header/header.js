a93fb83239b66aca81541dcb4382368b8ca819cd
Pavel Viarbitski <verbipo@gmail.com>
2022-12-16 13:55:14 +0100

    feat: update header style

--- src/modules/header/header.js
+++ src/modules/header/header.js
@@ -2,14 +2,16 @@ import './header.css';
 
 function Header() {
   return (
-    <header className="header">
-      <a href='#' className='logo link'><div className="logo-img"></div>Pavel Viarbbitski</a>
-      <nav className='nav'>
-        <li className='nav-item'><a href="#" className='link'>HOME</a></li>
-        <li className='nav-item'><a href="#" className='link'>ABOUT</a></li>
-        <li className='nav-item'><a href="#" className='link'>PROJECTS</a></li>
-        <li className='nav-item'><a href="#" className='link'>CONTACT</a></li>
-      </nav>
+    <header className='header'>
+      <div className='wrapper'>
+        <a href='#' className='logo link'><div className='logo-img'></div>Pavel Viarbitski</a>
+        <nav className='nav'>
+          <li className='nav-item'><a href='#hero' className='link'>HOME</a></li>
+          <li className='nav-item'><a href='#' className='link'>ABOUT</a></li>
+          <li className='nav-item'><a href='#' className='link'>PROJECTS</a></li>
+          <li className='nav-item'><a href='#' className='link'>CONTACT</a></li>
+        </nav>
+      </div>
 
     </header>
   );
