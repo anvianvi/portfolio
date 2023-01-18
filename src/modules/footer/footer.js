11eca32e55ead6036de13a45ecde1839c3d0af2c
Pavel Viarbitski <verbipo@gmail.com>
2023-01-18 18:54:07 +0100

    feat: footer block

--- src/modules/footer/footer.js
+++ src/modules/footer/footer.js
@@ -4,23 +4,21 @@ import { SOCIALMEDIA } from './social-list';
 function Footer() {
   return (
     <footer className='footer'>
+      <div className='footer-container content-wrapper'>
+        <div className='footer-centre'>
+          © Copyright
+          <script>document.write(new Date().getFullYear())</script>. Made by
+          <a href='https://anvianvi.github.io/portfolio/' className='copyright-link'> Pavel Viarbitski</a>
+        </div>
 
-      <div class="footer-centre">
-        © Copyright
-        <script>document.write(new Date().getFullYear())</script>. Made by
-        <a href="https://anvianvi.github.io/portfolio/">Pavel Viarbitski</a>
+        <div className='footer-social'>
+          {SOCIALMEDIA.map((media) => (
+            <a href={media.link} target='_blank' rel='noreferrer' className='foter-social-link' key={media.id}>
+              <img src={media.img} alt='social media icon' className='footer-social-link-img' />
+            </a>
+          ))}
+        </div>
       </div>
-
-      <div className='footer-social'>
-
-        {SOCIALMEDIA.map((media) => (
-          <a href={media.link} target="_blank" rel="noreferrer" className='foter-link-svg'>
-            <img src={media.img} alt="social media icon"/>
-          </a>
-        ))}
-
-      </div>
-
     </footer>
   );
 }
--- src/modules/footer/social-list.js
+++ src/modules/footer/social-list.js
@@ -2,19 +2,19 @@ const SOCIALMEDIA = [
 
   {
     id: 1,
-    img: require('./icons/maxresdefault.jpg'),
+    img: require('./icons/github.png'),
     name: "github",
     link: "https://github.com/anvianvi",
   },
   {
     id: 2,
-    img: require('./icons/linkedin.svg'),
+    img: require('./icons/linkedin.png'),
     name: "linkedin",
     link: "https://www.linkedin.com/in/pavel-viarbitsky/",
   },
   {
     id: 3,
-    img: require('./icons/codewars.svg'),
+    img: require('./icons/codewars.png'),
     name: "codewars",
     link: "https://www.codewars.com/users/anvianvi",
   },
