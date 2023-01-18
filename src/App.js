f9806e5270f0ebff080f9b97c7facab1f6ffc55a
Pavel Viarbitski <verbipo@gmail.com>
2023-01-18 18:55:39 +0100

    feat: view update, add eslint

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import About from './modules/main/about/about';
 import Hero from './modules/main/hero/hero';
 import Projects from './modules/main/projects/projects';
 import Footer from './modules/footer/footer';
+import Contacts from './modules/main/contacts/contacts';
 
 function App() {
   return (
     <>
       <Header />
-      <main className="main">
+      <main className='main'>
         <Hero />
         <About />
         <Projects />
+        <Contacts />
       </main>
       <Footer />
     </>
--- src/modules/main/projects/projects.js
+++ src/modules/main/projects/projects.js
@@ -9,11 +9,11 @@ function Projects() {
         <h2 className='h2'>Projects</h2>
         {PROJECTS.map((project) => (
           <div className='project-box' key={project.id}>
-            <div className='project-img-container'><img src={project.img} alt="screenshot of project main page" className='project-img' /></div>
+            <div className='project-img-container'><img src={project.img} alt='screenshot of project main page' className='project-img' /></div>
             <div className='project-box-text'>
               <div className='project-title'>{project.name}</div>
               <div className='project-description'>{project.description}</div>
-              <a href={project.deploy} className='btn' target="_blank" rel="noreferrer">Check deploy</a>
+              <a href={project.deploy} className='btn' target='_blank' rel='noreferrer'>Check deploy</a>
             </div>
           </div>
         ))}
