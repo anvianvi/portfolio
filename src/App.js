404ee0015da7050a2b305d83149bbb00bc50767f
Pavel Viarbitski <verbipo@gmail.com>
2023-01-20 19:56:16 +0100

    clean up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import Header from './modules/header/header';
-import About from './modules/main/about/about';
 import Hero from './modules/main/hero/hero';
+import About from './modules/main/about/about';
 import Projects from './modules/main/projects/projects';
-import Footer from './modules/footer/footer';
 import Contacts from './modules/main/contacts/contacts';
+import Footer from './modules/footer/footer';
 
 function App() {
   return (
