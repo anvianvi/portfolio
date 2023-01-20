90aa4447af1c7334a55cce5db2c3f47319911ce3
Pavel Viarbitski <verbipo@gmail.com>
2023-01-20 16:16:08 +0100

    feat: block about adaptive

--- src/modules/main/hero/hero.js
+++ src/modules/main/hero/hero.js
@@ -2,7 +2,7 @@ import './hero.css';
 
 function Hero() {
   return (
-    <section className='hero' id='hero'>
+    <section className='hero like-screen-height' id='hero'>
       <div className='hero-content'>
         <h1 className='h1'>Hi, My name is Pavel Viarbitski</h1>
         <div className='hero-info'>Frontend focused Web Developer with a passion to create beautiful and functional Interfaces and Web Applications that leads to the success of the overall product</div>
